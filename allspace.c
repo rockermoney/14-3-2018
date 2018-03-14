a90da70d322c7b2e022542f1eec9e854806c6c4e
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-03-14 15:47:34 +0530

    Create allspace.c

---
+++ allspace.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+int main(){
+    char inputString[1001], outputArray[1001];
+    int readIndex = 0, writeIndex;
+    printf("Enter the String \n");
+    gets(inputString);
+    while(inputString[readIndex] == ' '){
+    readIndex++;
+    }
+ for(writeIndex = 0;inputString[readIndex] != '\0'; readIndex++){
+      if(inputString[readIndex]==' ' && inputString[readIndex-1]==' '){
+          continue;
+      }
+      outputArray[writeIndex] = inputString[readIndex];
+      writeIndex++;
+    }
+    outputArray[writeIndex] = '\0';
+    printf("String without extra a spaces\n%s", outputArray);
+ getch();
+    return 0;
+}
